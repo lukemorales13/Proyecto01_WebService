2665b92a759cb0db1a2cf73f2a20f1a8ec3688ca
Luis Enrique Morales Flores <112268973+lukemorales13@users.noreply.github.com>
2023-09-20 20:00:25 -0600

    Update model.py
    Falta solucionar el caso de cuando se tiene que hacer una API call con el código IATA y no está en el caché.

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ key = "&appid=155505a47faf9082a7ee3d45f7b1ea0b&units=metric"
 url = "https://api.openweathermap.org/data/2.5/weather?" 
 cache = {}
 tickets = {}
+coordinates = {}
 data_csv = open('dataset2.csv')
 data_list = data_csv.readlines()
 data_list.pop(0)
@@ -37,6 +38,7 @@ def readData(data_list):
                 res1 = requests.get(url1)
                 data1 = res1.json()
                 cache[line[1]] = data1["weather"][0]
+                coordinates[f"{line[3]}, {line[4]}"] = data1["weather"][0]
                 time.sleep(1.5)
             except:
                 print(f"\nCould't request the weather information. The input {line} is probably incorrect.")
@@ -48,6 +50,7 @@ def readData(data_list):
                 res2 = requests.get(url2)
                 data2 = res2.json()
                 cache[line[2]] = data2["weather"][0]
+                coordinates[f"{line[5]}, {line[6]}"] = data1["weather"][0]
                 time.sleep(1.5)
             except:
                 print(f"\nCould't request the weather information. The input {line} is probably incorrect.")
@@ -56,29 +59,48 @@ def readData(data_list):
     return cache, tickets
     
 def searchWeatherWith_ticket(ticket):
-        if(ticket in tickets):
-            IATAS = tickets[ticket]
-            weather1 = cache[IATAS[0]]
-            weather2 = cache[IATAS[1]]
-            return weather1, weather2
-        else:
-            print("Ticket not found.\nPlease check again the information.")
+    if(ticket in tickets):
+        IATAS = tickets[ticket]
+        weather1 = cache[IATAS[0]]
+        weather2 = cache[IATAS[1]]
+        IATA1 = tickets[ticket][0]
+        IATA2 = tickets[ticket][1]
+        return (f"{IATA1}:\n{weather1}\n\n{IATA2}:\n{weather2}")
+    else:
+        return ("Ticket not found.\nPlease check again the information.")
+
+def searchWeatherWith_IATA(IATA):
+    if(IATA in cache):
+        return cache[IATA]
+    else:
+        return "O.o"
+        
+def searchWeatherWith_Coordinates(lat, lon):
+    if((f"{lat}, {lon}") in coordinates):
+        return coordinates[f"{lat}, {lon}"]
+    else:
+        url1 = (f"{url}lat={lat}&lon={lon}{key}")
+        res1 = requests.get(url1)
+        data1 = res1.json()
+        coordinates[f"{lat}, {lon}"] = data1["weather"][0]
+        return data1["weather"][0]
 
 cache, tickets = readData(data_list)    
 
+print(coordinates)
 print(tickets)
 for IATA in cache:
     print(f"{IATA}:\n{cache[IATA]}")
     
 while True:
-    ticket = input("Insert ticket: ")
-    if(ticket in tickets):
-        weather1, weather2 = searchWeatherWith_ticket(ticket)
-        IATA1 = tickets[ticket][0]
-        IATA2 = tickets[ticket][1]
-        print(f"{IATA1}:\n{weather1}\n\n{IATA2}:\n{weather2}")
-    else:
-        print("Ticket not found.\n")
-        
-    if (input("Do you want to make another search? yes | no: ")=="no"):
+    search = input(f"Search with: ticket=0 | iata=1 | coordinates=2\n")
+    if(search=="0"):
+        print(searchWeatherWith_ticket(input("Insert ticket:\n")))
+    elif(search=="1"):
+        print(searchWeatherWith_IATA(input("Insert IATA code:\n")))
+    elif(search=="2"):
+        lat = input("Insert latitude: ")
+        lon = input("Insert longitude: ")
+        print(searchWeatherWith_Coordinates(lat, lon))
+    if (input("\nDo you want to make another search? yes | no: ")=="no"):
         break
