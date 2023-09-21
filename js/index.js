2286aa2755986c4100010ee644998d9e35e8d376
Alejandro-Estrada-1 <alejandro.sanchez.estrada@ciencias.unam.mx>
2023-09-21 00:22:28 -0600

    avanzando ando

--- js/index.js
+++ js/index.js
@@ -1,27 +1,37 @@
-const weatherApiUrl = "httpf://api.openwathermap.org/data2.5/weather?";
-const weatherKey = "&appid=155505a47faf9082a7ee3d45f7b1ea0b&units=metric";
+// Función para hacer la solicitud al servidor Python y mostrar los datos en la tarjeta (card)
+function updateWeatherCard(data) {
+  const temperatureSpan = document.getElementById("temperature");
+  const descriptionSpan = document.getElementById("description");
 
-// Función para hacer la solicitud a model.py
-function getWeatherForTicket(ticket) {
-  fetch(`${weatherApiUrl}?ticket=${ticket}`)
-    .then(response => response.json())
-    .then(data => {
-      // Maneja los datos meteorológicos recibidos (data) aquí
-      const weatherInfoDiv = document.getElementById("weather-info");
-
-      weatherInfoDiv.innerHTML = `
-                <h2>Datos Meteorológicos</h2>
-                <p>Temperatura: ${data.temperature}°C</p>
-                <p>Descripción: ${data.description}</p>
-            `;
-    })
-    .catch(error => {
-      console.error("Error al obtener datos meteorológicos:", error);
-    });
+  temperatureSpan.textContent = data.weather1.main.temp + "°C";
+  descriptionSpan.textContent = data.weather1.weather[0].description;
 }
 
-// Ejemplo de cómo usar la función para obtener datos meteorológicos para un boleto específico
-const ticketNumber = "ABC123";
-getWeatherForTicket(ticketNumber);
+// Llama a la API para obtener los datos meteorológicos cuando se hace clic en el botón
+function getWeatherForTicket() {
+  const ticketInput = document.getElementById("ticket-input");
+  const ticketNumber = ticketInput.value.trim();
 
+  if (ticketNumber === "") {
+      alert("Por favor, ingresa un número de boleto válido.");
+      return;
+  }
 
+  fetch(`/get_weather?ticket=${ticketNumber}`)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`Error de red: ${response.status}`);
+          }
+          return response.json();
+      })
+      .then(data => {
+          updateWeatherCard(data);
+      })
+      .catch(error => {
+          console.error("Error al obtener datos meteorológicos:", error);
+      });
+}
+
+// Asociar la función getWeatherForTicket al evento clic del botón de búsqueda
+const searchButton = document.getElementById("search-button");
+searchButton.addEventListener("click", getWeatherForTicket);
