9d0556b1331106721ba1652353b3d98ecd85419c
Sandra <sandrarivera@ciencias.unam.mx>
2023-09-22 12:00:58 -0600

    levenshtein funciona para iata y su ciudad

--- levenshtein.py
+++ levenshtein.py
@@ -58,10 +58,52 @@ def import_pkl(pkl_file):
         location_data = pickle.load(file_in)
     return location_data
 
-def calculate_distance(word, location_data):
+def calculate_distance(word, num_words, max_level, type, location_data):
+    dict_distance = {}
+    for location in location_data:
+        location_distance = int(levenshtein_distance(word, location[type]))
+        if (location_distance <= max_level):
+            if not location_distance in dict_distance:
+                dict_distance[location_distance] = []
+            dict_distance[location_distance].append(location)
+
+#    print(dict_distance.keys())
+#    print(dict_distance)
+
+    return closest_words(num_words, 1, max_level, dict_distance, [])
+        
+def closest_words(num_words, level, max_level, dict_distance, closest_locations):
+    if(level <= max_level):
+        if level in dict_distance:
+            dict_distance[level].sort()
+            for location in dict_distance[level]:
+                closest_locations.append(location)
+                num_words = num_words - 1
+                if (num_words == 0):
+                    return closest_locations
+        return closest_words(num_words, level+1, max_level, dict_distance, closest_locations)
+    return closest_locations
+
+def short_search(word, num_words, max_distance, type):
+    iata_list = import_pkl('iata_list.pkl')
+    return calculate_distance(word, num_words, max_distance, type, iata_list)
+
+def iata_search(word):
+    print('IATA ##########')
+    return short_search(word, 5, 1, 0)
+
+def city_search(word):
+    print('CITY ##########')
+    return short_search(word, 5, 4, 1)
+
+def massive_search(word, num_words):
+    print('MASSIVE ##########')
+    cities_dictionary = import_pkl('ciudades.pkl')
+
+    location_data = cities_dictionary[normalize_name(word)[0]]
+
     list_distance = []
     wordIdx = 0
-
 ######## Ver excepcion si la primera letra no esta #########
     for city in location_data:
         word_distance = levenshtein_distance(word, city)
@@ -69,30 +111,22 @@ def calculate_distance(word, location_data):
             word_distance = 9
         list_distance.append(str(int(word_distance)) + "-" + city)
         wordIdx = wordIdx + 1
-    
-    return list_distance
 
-def closest_words(num_words, location_data, list_distance):
     closestWords = {}
     wordDetails = []
     currWordDist = 0
     list_distance.sort()
-
     for i in range(num_words):
         currWordDist = list_distance[i]
         wordDetails = currWordDist.split("-")
         closestWords[wordDetails[1]] = location_data[wordDetails[1]]
-
     return closestWords 
 
 
-def massive_search(word, num_Words):
-    cities_dictionary = import_pkl('ciudades.pkl')
-    cities_distance = calculate_distance(word, cities_dictionary[normalize_name(word)[0]])
-    return closest_words(num_Words, cities_dictionary[normalize_name(word)[0]], cities_distance)
-
-
-
-city = "Monterrey"
-print(massive_search(city, 10))
+location = "Monterrey"
+print(massive_search(location, 10))
+print()
+print("iata search: ", iata_search('MY'))
+print()
+print("city search: ", city_search('Vercruz'))
 print()
