1f1674b75acd96b6c5b3e46a2207731bae8e590d
Alejandro-Estrada-1 <alejandro.sanchez.estrada@ciencias.unam.mx>
2023-09-24 00:08:55 -0600

    cccc

--- src/app/static/js/index.js
+++ src/app/static/js/index.js
@@ -9,47 +9,92 @@ function updateWeatherCard(data) {
   const temperatureSpan = document.getElementById("temperature");
   const descriptionSpan = document.getElementById("description");
 
-  temperatureSpan.textContent = data.weather1.main.temp + "°C";
-  descriptionSpan.textContent = data.weather1.weather[0].description;
-
-  if (data.weather2) {
-    // Si hay datos para la segunda ciudad, mostrarlos
-    temperatureSpan.textContent += " / " + data.weather2.main.temp + "°C";
-    descriptionSpan.textContent += " / " + data.weather2.weather[0].description;
-  }
+  temperatureSpan.textContent = data.temperature + "°C";
+  descriptionSpan.textContent = data.description;
 }
 
-// Llama a la API para obtener los datos meteorológicos cuando se hace clic en el botón
-function getWeatherForTicket() {
-  const ticketInput = document.getElementById("search-input");
-  const ticketNumber = ticketInput.value.trim();
-
-  if (ticketNumber === "") {
-      alert("Por favor, ingresa un número de boleto válido.");
-      return;
-  }
-
-  fetch(`/get_weather?ticket=${ticketNumber}`)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error(`Error de red: ${response.status}`);
-          }
-          return response.json();
-      })
-      .then(data => {
-          updateWeatherCard(data);
-      })
-      .catch(error => {
-          console.error("Error al obtener datos meteorológicos:", error);
-      });
-}
-
-searchButton.addEventListener("click", function () {
-  const inputValue = document.getElementById("search-input").value.trim();
+// Llama a las funciones de búsqueda y muestra los resultados en la tarjeta cuando se hace clic en el botón "Search"
+function searchByIata() {
+  const inputElement = document.getElementById("search-input");
+  const inputValue = inputElement.value.trim();
 
   if (inputValue === "") {
-    alert("Por favor, ingresa un número de boleto o una ciudad válida.");
+    alert("Por favor, ingresa un IATA code válido.");
     return;
   }
-  
-}); 
+
+  // Realiza la búsqueda por IATA code utilizando la función de búsqueda correspondiente
+  fetch(`/iata_search?input=${inputValue}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error de red: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      updateWeatherCard(data);
+    })
+    .catch(error => {
+      console.error("Error al obtener datos meteorológicos:", error);
+    });
+}
+
+function searchByCity() {
+  const inputElement = document.getElementById("search-input");
+  const inputValue = inputElement.value.trim();
+
+  if (inputValue === "") {
+    alert("Por favor, ingresa un nombre de ciudad válido.");
+    return;
+  }
+
+  // Realiza la búsqueda por nombre de ciudad utilizando la función de búsqueda correspondiente
+  fetch(`/city_search?input=${inputValue}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error de red: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      updateWeatherCard(data);
+    })
+    .catch(error => {
+      console.error("Error al obtener datos meteorológicos:", error);
+    });
+}
+
+function searchByCoordinates() {
+  const inputElement = document.getElementById("search-input");
+  const inputValue = inputElement.value.trim();
+
+  if (inputValue === "") {
+    alert("Por favor, ingresa coordenadas válidas.");
+    return;
+  }
+
+  // Realiza la búsqueda por coordenadas utilizando la función de búsqueda correspondiente
+  fetch(`/massive_search?input=${inputValue}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error de red: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      updateWeatherCard(data);
+    })
+    .catch(error => {
+      console.error("Error al obtener datos meteorológicos:", error);
+    });
+}
+
+// Asociar las funciones de búsqueda a los eventos clic de los botones correspondientes
+const iataSearchButton = document.getElementById("iata-search-button");
+iataSearchButton.addEventListener("click", searchByIata);
+
+const citySearchButton = document.getElementById("city-search-button");
+citySearchButton.addEventListener("click", searchByCity);
+
+const coordinatesSearchButton = document.getElementById("coordinates-search-button");
+coordinatesSearchButton.addEventListener("click", searchByCoordinates);
